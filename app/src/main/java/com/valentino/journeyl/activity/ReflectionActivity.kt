9fc602dc5c67e44c97587320550cce009ad3c046
Leyton Tsai <phtsai2@illinois.edu>
2018-04-20 13:49:22 -0500

    --amend

--- app/src/main/java/com/valentino/journeyl/activity/ReflectionActivity.kt
+++ app/src/main/java/com/valentino/journeyl/activity/ReflectionActivity.kt
@@ -2,6 +2,7 @@ package com.valentino.journeyl.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.valentino.journeyl.R
 import com.valentino.journeyl.dao.MilestoneDAO
 import com.valentino.journeyl.model.Goal
@@ -9,6 +10,7 @@ import com.valentino.journeyl.model.Milestone
 import kotlinx.android.synthetic.main.activity_reflection.*
 
 class ReflectionActivity : AppCompatActivity() {
+    var levels = arrayOf("Beginner", "Intermediate", "Advanced")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,11 +20,14 @@ class ReflectionActivity : AppCompatActivity() {
         goalName.text = goal.description
         milestoneName.text = milestone.description
 
+        ratingFourSpinner.adapter = ArrayAdapter(this,
+                                                android.R.layout.simple_spinner_dropdown_item, levels)
+
         submitButton.setOnClickListener({
             milestone.rating1 = ratingOneSeekbar.progress
             milestone.rating2 = ratingTwoSeekbar.progress
             milestone.rating3 = ratingThreeSeekbar.progress
-            milestone.rating4 = "TODDO"
+            milestone.rating4 = ratingFourSpinner.selectedItem?.toString()
             milestone.reflection = reflectionEditText.text.toString()
             MilestoneDAO.postReflection(milestone)
             finish()
